d3b4bca35ef018c10d8138205ebcaf5a2996b816
yongyecc <1014885794@qq.com>
2022-09-10 16:27:21 +0800

    Add ListView replace button to tigger Demo

--- handler/src/main/java/cn/yongye/handler/MainActivity.java
+++ handler/src/main/java/cn/yongye/handler/MainActivity.java
@@ -3,10 +3,23 @@ package cn.yongye.handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+import cn.yongye.handler.common.Config;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +27,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
 
-        Button button = findViewById(R.id.updateUI);
-        button.setOnClickListener(new View.OnClickListener() {
+        ArrayList<String> demos = new ArrayList<>();
+        demos.add("动态更新Activity界面");
+        ItemAdapter itemAdapter = new ItemAdapter(this, demos);
+        listView.setAdapter(itemAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName("cn.yongye.handler", "cn.yongye.handler.demo.UpdateUIThread"));
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, String.format("Click %s item.", i),
+                        Toast.LENGTH_SHORT).show();
+                switch (i){
+                    case 0:
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName("cn.yongye.handler", "cn.yongye.handler.demo.UpdateUIThread"));
+                        startActivity(intent);
+                        break;
+                }
             }
         });
+    }
+
+    public class ItemAdapter extends BaseAdapter{
+
+        ArrayList<String> mData  = null;
+        Context mContext = null;
+
+        public ItemAdapter(Context context, ArrayList<String> mData) {
+            this.mContext = context;
+            this.mData = mData;
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.list_item, viewGroup, false);
+            TextView nameView = view.findViewById(R.id.DemoName);
+            nameView.setText(mData.get(i));
+            return view;
+        }
     }
 }
